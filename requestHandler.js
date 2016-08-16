b76e7e1ea41f986da85b6f7cb340c5897ec8c914
kraghot <edi.cikovic@gmail.com>
2016-08-16 18:17:20 +0200

    Response handling

--- requestHandler.js
+++ requestHandler.js
@@ -1,12 +1,18 @@
-function start()
+function start(response)
 {
 	console.log("Start page");
+	response.writeHead(200, {"content-type": "text/plain"});
+	response.write("Start page");
+	response.end();
 }
 
 
-function upload()
+function upload(response)
 {
 	console.log("Upload Page");
+	response.writeHead(200, {"content-type": "text/plain"});
+	response.write("Upload page");
+	response.end();
 }
 
 
--- router.js
+++ router.js
@@ -1,14 +1,18 @@
+var util = require("util");
+
 function route(handle, pathname, response)
 {
 	console.log("URL : " + pathname);
-	if(typeof(handle[pathname]) === function) 
+	if(typeof(handle[pathname]) === 'function')
 	{
 		handle[pathname](response);
 	}
-	else 
+	else
 	{
-		util.log("Unrecognized handle " + pathname);
-
+		util.log("404 Unrecognized handle " + pathname);
+		response.writeHead(404, {"content-type" :"text-plain"});
+		response.write("ERROR\n 404 Not Found");
+		response.end();
 	}
 }
 
